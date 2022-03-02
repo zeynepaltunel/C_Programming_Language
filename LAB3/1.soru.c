492634e546560a86df39189656ce21ec0fc88cf1
Zynp-Altnl <zeynepaltunel23@gmail.com>
2022-03-02 22:57:03 +0300

    Add files via upload

---
+++ LAB3/1.soru.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int func(int[], int, int);
+int main(){
+	int a[100];
+	int n,i,x, b=0;
+	
+	printf("Write a size: \n");
+	scanf("%d", &n);
+	printf("Write numbers: \n");
+	
+	for(i=0; i<n; i++){
+		scanf("%d", &a[i]);
+	}
+	x=func(a,n-1,b);
+	printf("%d", x);
+	return 0;
+}
+
+int func(int a[], int n ,int b){
+	
+	if(n<0)
+		return b;
+	if(a[n]%2==1)
+		b++;
+	
+	return func(a,--n,b);		
+}
